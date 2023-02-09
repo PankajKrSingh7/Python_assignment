086940dcfc51cd2d2fd49f5d8420663e856d23a6
Pankaj Kumar Singh <54628129+PankajKrSingh7@users.noreply.github.com>
2023-02-09 21:42:57 +0530

    Update surrounded-regions.py

--- surrounded-regions.py
+++ surrounded-regions.py
@@ -1,52 +1,45 @@
-board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
-print(f"Original Matrix: {board}")
 
-# we can also take the matrix as user input 
-'''
-m = int(input("Number of rows:"))
-n = int(input("Number of columns:"))
+def capturing_using_recursion(r,c,rows,cols): 
+        if (r<0 or r==rows or c<0 or c==cols or board[r][c]!="O"):
+            return 
+        board[r][c]="#"
+        capturing_using_recursion(r+1,c,rows,cols)
+        capturing_using_recursion(r-1,c,rows,cols)
+        capturing_using_recursion(r,c+1,rows,cols)
+        capturing_using_recursion(r,c-1,rows,cols)
 
-board=[]
+def solve(board):
+        rows=len(board) 
+        cols=len(board[0])
 
-for i in range(m):
-    data = []
-    print("Enter",i," row data")
-    for j in range(n):
-        s = input()
-        data.append(s)
-        
-    board.append(data)
-'''
-# m and n are row and columns of board respectively
-m = len(board)
-n = len(board[0])
+        #(DFS)capture unsurrounded region(0->#) starting from boundry Os 
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="O" and (r==0 or r==rows-1 or c==0 or c==cols-1):
+                    capturing_using_recursion(r,c,rows,cols)
 
-# DFS algorithm to move on the Board and make changes 
-def dfs(board,r,c):
-    if(r<=0 or c<0 or r>=len(board) or c>=len(board[0]) or board[r][c]=='X'):
-        return
-    board[r][c] ='#'
-    dfs(board,r+1,c)
-    dfs(board,r-1,c)
-    dfs(board,r,c+1)
-    dfs(board,r,c-1)
+        #capture remaining  surrounded region(0->X)
+        for r in range(1,rows-1):
+            for c in range(1,cols-1):
+                if board[r][c]=="O":
+                    board[r][c]="X" 
 
-# Running DFS on boundry 'O' and changing them to '#' since it is not surrounded by 'X'
-for r in range(m):
-    for c in range(n):
-        if(board[r][c] == 'O' and (r in [0,m-1] or c in [0,n-1])):
-            dfs(board,r,c)
+        #uncapture unsurrounded region from # to O(#->0)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="#":
+                    board[r][c]="O"
 
-# Now all remaining 'O' are surrounded by 'X' so we change them to 'X'
-for r in range(m):
-    for c in range(n):
-        if(board[r][c] == 'O'):
-            board[r][c] = 'X'
 
-# Here converting '#' back to 'O'           
-for r in range(m):
-    for c in range(n):
-        if(board[r][c] == '#'):
-            board[r][c] = 'O'
 
-print(f"Matrix After Capture: {board}")            
+# Driver code
+if __name__ == '__main__':
+ 
+    board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+    print(f"Original Matrix: {board}")
+ 
+    (board)
+
+    print(f"Matrix After Capture: {board}") 
+ 
+    
